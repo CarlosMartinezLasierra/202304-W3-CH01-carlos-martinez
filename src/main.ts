ee6742c15c7c7045a7cf2de1e25792dba4d3eb98
Carlos Martinez <carlosmartinezlasierra@gmail.com>
2023-05-17 06:43:08 +0200

    Delete challenge1 extra folder

---
+++ src/main.ts
@@ -0,0 +1,30 @@
+/* eslint-disable no-new */
+import { createCharacterList } from "./character-list.ts";
+import { List } from "./component/list.ts";
+import { Character } from "./component/character-component.ts";
+
+import "./style.css";
+import { Comunication } from "./component/comunication.ts";
+import { killCharacter, makeCharacterSpeak } from "./character-actions.ts";
+
+const characterList = createCharacterList();
+
+new List(".app");
+characterList.forEach((item) => {
+  new Character(".characters-list", item);
+});
+
+new Comunication("body");
+
+const buttons = document.querySelectorAll(".btn") as any;
+
+buttons.forEach((button: HTMLButtonElement) => {
+  button.addEventListener("click", (event: Event) => {
+    const element = event.target as HTMLButtonElement;
+    if (element.classList.contains("die")) {
+      killCharacter(element, characterList);
+    } else {
+      makeCharacterSpeak(element, characterList);
+    }
+  });
+});
